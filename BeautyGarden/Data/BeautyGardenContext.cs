e5f9591bf2322005afba5a9e5eef1bbfdd4571d6
Bryan Jacobs <bcrimj@gmail.com>
2021-02-07 13:43:21 -0800

    Test

---
+++ BeautyGarden/Data/BeautyGardenContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BeautyGarden.Models;
+
+namespace BeautyGarden.Data
+{
+    public class BeautyGardenContext : DbContext
+    {
+        public BeautyGardenContext (DbContextOptions<BeautyGardenContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<BeautyGarden.Models.Contact> Contact { get; set; }
+    }
+}
