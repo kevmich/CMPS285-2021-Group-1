c946beb30a2f024078233b37b9bf4c5b8ed86d10
Kevmich <kevin.ziebarth@selu.edu>
2021-02-09 17:43:03 -0600

    Your turn. branch off of this on get to work

---
+++ BeautyGardenMain/BeautyGardenMain/Controllers/UserContactController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeautyGardenMain.Data;
+using BeautyGardenMain.Entity.UserContacts;
+
+namespace BeautyGardenMain.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserContactController : ControllerBase
+    {
+        private readonly DataContext dataContext;
+
+        public UserContactController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpPost]
+        public ActionResult<UserContactDto> Created(UserContactDto dto)
+        {
+            var data = dataContext.Set<UserContact>().Add(new UserContact
+            {
+                FullName = dto.FullName,
+                PhoneNumber = dto.PhoneNumber,
+                Email = dto.Email,
+                Comment = dto.Comment
+            });
+            dataContext.SaveChanges();
+            return Created(string.Empty, dto);
+
+        }
+
+    }
+}
---
+++ BeautyGardenMain/BeautyGardenMain/Entity/UserContacts/UserContact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeautyGardenMain.Entity.UserContacts
+{
+    public class UserContact
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+    }
+}
