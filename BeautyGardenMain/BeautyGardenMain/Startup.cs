63d9f082030a299b188a181a903d336dd21ff8de
Kevmich <kevin.ziebarth@selu.edu>
2021-02-09 15:58:55 -0600

    reset

--- BeautyGardenMain/BeautyGardenMain/Startup.cs
+++ BeautyGardenMain/BeautyGardenMain/Startup.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.AspNetCore.Builder;
+using BeautyGardenMain.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using BeautyGardenMain.Data;
-using BeautyGardenMain.Models;
 
 namespace BeautyGardenMain
 {
@@ -26,8 +27,12 @@ namespace BeautyGardenMain
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationUserContact>(options => options.UseSqlServer(Configuration.GetConnectionString("Myconnection")));
-            services.AddControllersWithViews();
+            services.AddControllers();
+
+            services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DataContext")));
+
+            services.AddSwaggerGen();
 
         }
 
@@ -38,14 +43,17 @@ namespace BeautyGardenMain
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -53,9 +61,7 @@ namespace BeautyGardenMain
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
