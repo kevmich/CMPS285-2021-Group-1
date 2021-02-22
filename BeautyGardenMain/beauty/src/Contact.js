1d302ea3dcab6983232d473a59513e9671fd6efb
Bryan Jacobs <bcrimj@gmail.com>
2021-02-22 15:48:38 -0800

    ContactPage

--- BeautyGardenMain/beauty/src/Contact.js
+++ BeautyGardenMain/beauty/src/Contact.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ContactForm from "./Components/ContactForm";
 
 function Contact(){
     return(
         <div>
             <h1>Contact us!</h1>
+            <ContactForm />
         </div>
 
     );
