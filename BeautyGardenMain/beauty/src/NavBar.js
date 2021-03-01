d7641673bbf4d4a4f7889eb8b69f83af598b5ca8
Ximena <77812328+ximefab@users.noreply.github.com>
2021-02-28 18:06:11 -0600

    changes w thomas

--- BeautyGardenMain/beauty/src/NavBar.js
+++ BeautyGardenMain/beauty/src/NavBar.js
@@ -16,10 +16,6 @@ class NavBar extends Component{
 
                 </div>
                 <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-
                     
                     {MenuItems.map((item, index) => {
                         return (
