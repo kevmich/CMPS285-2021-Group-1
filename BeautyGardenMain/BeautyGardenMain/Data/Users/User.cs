4a18464c3430b9178ad6e79352646a3018462e32
pudgee123 <71963350+pudgee123@users.noreply.github.com>
2021-03-06 21:39:13 -0600

    Post,Role,User,Roles,UserRole.

---
+++ BeautyGardenMain/BeautyGardenMain/Data/Users/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeautyGardenMain.Data.Users
+{
+    public class User : IdentityUser<int>
+    {
+        public virtual ICollection<UserRole> Roles { get; set; } = new List<UserRole>();
+    }
+}
