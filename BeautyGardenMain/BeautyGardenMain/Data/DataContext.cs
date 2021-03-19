45aca27dfd657883d7c05dbd2df8d5a2dcbd0930
pudgee123 <71963350+pudgee123@users.noreply.github.com>
2021-03-19 18:24:56 -0500

    Deletion of unnecessary constructor.

--- BeautyGardenMain/BeautyGardenMain/Data/DataContext.cs
+++ BeautyGardenMain/BeautyGardenMain/Data/DataContext.cs
@@ -14,10 +14,7 @@ namespace BeautyGardenMain.Data
     public class DataContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
 
-        public DataContext()
-        {
-
-        }
+      
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
