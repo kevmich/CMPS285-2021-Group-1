4a9dfbc1096111cf594b9be16c3da88b93a0677f
pudgee123 <71963350+pudgee123@users.noreply.github.com>
2021-03-23 16:11:48 -0500

    Login migrations

--- BeautyGardenMain/BeautyGardenMain/Data/DataContext.cs
+++ BeautyGardenMain/BeautyGardenMain/Data/DataContext.cs
@@ -21,7 +21,7 @@ namespace BeautyGardenMain.Data
         }
         public DbSet<Post> Post { get; set; }
         public DbSet<UserContact> UserContact { get; set; }
-        //public DbSet<Post> Posts { get; set; }
+       
         //DbSet go here--------
 
         protected override void OnModelCreating(ModelBuilder builder)
