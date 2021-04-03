016c86f065f76d757d0a591c1be533a1ae95c596
Ximena <77812328+ximefab@users.noreply.github.com>
2021-04-03 02:59:17 -0500

    New Services files

---
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/ServicesTab/FaceNWax.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FaceNWax.css'
+
+const FaceNWax = () => {
+    return (
+        <body>
+            <div>
+                Lip filler page
+            </div>
+     </body>
+    )
+}
+
+export default FaceNWax;
---
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/ServicesTab/FatDissolve.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FatDissolve.css'
+
+const FatDissolve = () => {
+    return (
+        <body>
+            <div>
+                Lip filler page
+            </div>
+     </body>
+    )
+}
+
+export default FatDissolve;
