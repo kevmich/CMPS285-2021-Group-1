c496af7fb87de3d6ba10947ec4c33cc28ba35804
Ximena <77812328+ximefab@users.noreply.github.com>
2021-04-02 22:50:32 -0500

    services tab link outlines and names

--- BeautyGardenMain/BeautyGardenMain/beauty/src/Navigation.js
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/Navigation.js
@@ -33,42 +33,80 @@ const Navigation = (props) => {
         <NavbarBrand href="/" title="Go to the homepage">The Beauty Garden</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
+
+
           <Nav className="mr-auto" navbar>
+
             <NavItem>
               <NavLink href="/Home/" title="Go to the homepage">Home</NavLink>
             </NavItem>
+
+
+
             <NavItem>
-              <NavLink href="/Services" title="See what we offer">Services</NavLink>
+              
             </NavItem>
+
+
+
             <NavItem>
               <NavLink href="/About/" title="Learn about us">About</NavLink>
             </NavItem>
+
+
+
             <NavItem>
               <NavLink href="/Contact/" title="Contact us">Contact</NavLink>
             </NavItem>
+
+
+
             <NavItem>
               <NavLink href="/ListPage/" title="View all inquiries">Contact List</NavLink>
             </NavItem>
-            <NavItem></NavItem>
+
+
             <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav caret>
-                Meet The team
+                Services
               </DropdownToggle>
+
+            
               <DropdownMenu right>
+
+              
                 <DropdownItem>
-                  Name1
+                <NavLink href="/Services/" title="See what we offer">Lash Extensions</NavLink>
                 </DropdownItem>
+
                 <DropdownItem>
-                  Name2
+                <NavLink href="/Services/" title="See what we offer">Teeth Treatments</NavLink>
                 </DropdownItem>
-                <DropdownItem divider />
+
                 <DropdownItem>
-                  Reset
+                <NavLink href="/Services/" title="See what we offer">Needle Free Lip Fillers</NavLink>
                 </DropdownItem>
+               
+                <DropdownItem>
+                <NavLink href="/Services/" title="See what we offer">Needle Free Fat Dissolve</NavLink>
+                </DropdownItem>
+
+                <DropdownItem>
+                <NavLink href="/Services/" title="See what we offer">Facials and Waxing</NavLink>
+                </DropdownItem>
+
+
+
               </DropdownMenu>
             </UncontrolledDropdown>
+
           </Nav>
-          <NavbarText>Coming soon!</NavbarText>
+
+
+          <NavbarText>
+            Login Button Here!
+
+          </NavbarText>
          
           
         </Collapse>
