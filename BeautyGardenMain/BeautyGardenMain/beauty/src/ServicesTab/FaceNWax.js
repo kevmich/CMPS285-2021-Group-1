6deb4fbaf5352cd6179a6e082ee1d1bcb20c2026
Ximena <77812328+ximefab@users.noreply.github.com>
2021-04-07 17:10:27 -0500

    faceNwax outline

--- BeautyGardenMain/BeautyGardenMain/beauty/src/ServicesTab/FaceNWax.js
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/ServicesTab/FaceNWax.js
@@ -3,9 +3,35 @@ import './FaceNWax.css'
 
 const FaceNWax = () => {
     return (
-        <body>
-            <div>
-                Face and Wax page
+        <body className="bodyColor">
+            <div className="FaceWaxTitles">
+                <h2>Face services</h2>
+            </div>
+
+            <div className="FaceContainer">
+                <div className="FaceleftBox">
+
+                </div>
+
+                <div className="FacerightBox">
+
+                </div>
+
+            </div>
+
+            <div className="FaceWaxTitles">
+                <h2> Wax Titles</h2>
+            </div>
+
+            <div className="WaxContainer">
+                <div className="WaxleftBox">
+                
+                </div>
+
+                <div className="WaxrightBox">
+                    
+                </div>
+
             </div>
      </body>
     )
