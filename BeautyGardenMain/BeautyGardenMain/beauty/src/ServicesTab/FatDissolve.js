445d45d69ef31da01317ab97d3af90bd49ea8c26
Ximena <77812328+ximefab@users.noreply.github.com>
2021-04-12 00:58:40 -0500

    Service Tab text

--- BeautyGardenMain/BeautyGardenMain/beauty/src/ServicesTab/FatDissolve.js
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/ServicesTab/FatDissolve.js
@@ -3,7 +3,7 @@ import './FatDissolve.css'
 
 const FatDissolve = () => {
     return (
-        <body>
+        <body className="DissolveBody">
             <div className="DissolveMainTitle">
                 <h2>Fat dissolve Treatment</h2>
             </div>
@@ -11,10 +11,27 @@ const FatDissolve = () => {
             <div className="BoxesContainer">
 
                 <div className="DissleftBox">
-
+                    [image goes here]
                 </div>
 
                 <div className="DissrightBox">
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <h1>Fat Dissolve Treatment</h1>
+                    <h4>Kybella is a product made of deoxycholic acid, which is a naturally occurring 
+                        molecule that aids in the breakdown and absorption of fat.
+                        This is the only FDA-Approved treatment that destroys fat cells permanently! </h4>
 
                 </div>
 
@@ -26,7 +43,28 @@ const FatDissolve = () => {
             </div>
             
             <div className="DissolveFAQ">
+                <br></br>
+                <br></br>
+                <h3>How many sessions will I need to notice a difference?</h3>
+                <h5>You will be able to notice results after the first session. We recommend 2-4 sessions, depending on your goals. </h5>
 
+                <br></br>
+                <br></br>
+                <h3>How is it needle free? Does it hurt?</h3>
+                <h5>We use a hyaluron pen for our procedures instead of a needle! </h5>
+                <h5>A hyaluron pen is a pressurized device that inserts the filler into the skin. This procedure does not hurt at all, actually!</h5>
+
+                <br></br>
+                <br></br>
+                <h3>Side Effects? </h3>
+                <h5>Being a naturally occurring substance, the chances for any side effects are VERY rare and almost impossible. </h5>
+                <h5>You will notice small pinpoint dots where the filler was inserted. These will subside in about 3 days after your procedure. </h5>
+                <h5>You may use makeup to cover them in needed as well!</h5>
+
+                <br></br>
+                <br></br>
+                <h3>How long will the results last?</h3>
+                <h5>Kybella permanently dissolves the fat. Your results will last forever with proper diet and upkeep! </h5>                               
             </div>
      </body>
     )
