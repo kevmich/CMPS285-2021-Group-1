6d0b7f4c6c336cf3e7c5cde57be77bc2241aa632
Cameron Branch <cameron.branch@selu.edu>
2021-04-13 09:12:46 -0500

    Fixed Navbar formatting

---
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/Buttons/mainButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './mainButton.css';
+import {Link} from 'react-router-dom';
+
+export function mainButton() {
+    return (
+        <button className='mainBtn'></button>
+    );
+}
+
+export default mainButton;
