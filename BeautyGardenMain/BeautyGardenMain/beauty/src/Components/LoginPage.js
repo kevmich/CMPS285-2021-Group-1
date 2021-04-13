65db1a7968a6b43e564f3e35f6bc0c122fdc2be0
pudgee123 <71963350+pudgee123@users.noreply.github.com>
2021-04-13 13:58:00 -0500

    new login page

--- BeautyGardenMain/BeautyGardenMain/beauty/src/Components/LoginPage.js
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/Components/LoginPage.js
@@ -1,42 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import Login from '../Login';
-import useForm from '../useForm'
+import React, { useState } from 'react';
 import './LoginPage.css';
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import axios from 'axios';
 
-const LoginPage = () => {
-    const { handleChange, newContact, handleSubmit, errors } = useForm();
-   
+const Schema = yup.object().shape({
+    userName: yup
+    .string()
+    .required()
+    .min(3),
+
+    password: yup
+    .string()
+    .required()
+    .min(6),
+});
+
+
+
+    function LoginPage() {
+      const{handleSubmit, register, setError} = useForm({validationSchema: Schema});
+
+        function onSubmit(formData) {
+                console.log(formData);
+        }
+
+        console.log(setError)
 
     
-    
-        
 
-       
-    
+        return (
+            
+            <div className="container" >
+                <form className='white' onSubmit={handleSubmit(onSubmit)}>
+                    <body>                 
+                
+                      <div className="input-field">
+                            <label htmlFor="userName">User Name</label>
+                            <input type="text" name="userName" placeholder="example@yahoo.com" {...register('userName')} />
+                           {setError.userName && <p>{setError.userName.message}</p>}
+                         </div>   
 
-    return (
-        
-        <div className="container" >
-            <form className='white' onSubmit={handleSubmit}>
-                <body>                 
-             
-                    <div className="input-field" >
-                    <label htmlFor="email">Email</label>
-                        <input type="text" name="email" placeholder="example@yahoo.com" value={newContact.email} onChange={handleChange}  />
-                        <div style ={{ color: "red "}}>{newContact.emailError}</div>
-                        </div>
                         <div className="input-field">
-                    <label htmlFor="password">Password</label>
-                        <textarea type="text" name="comment" placeholder ="Please enter your password" value={newContact.comment}
-                            styles={{width:"300px"}} onChange={handleChange}  />
-                            <div style ={{ color: "red "}}>{newContact.commentError}</div>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" placeholder ="Please enter your password"{...register('password')}
+                                styles={{width:"300px"}} />
+                            {setError.password && <p>{setError.password.message}</p>}
+                            </div>
+
+                        <div>
+                            <button className="btn blue darken-3" type="submit">Submit</button>
                         </div>
-                    <div>
-                        <button className="btn blue darken-3" type="submit">Submit</button>
-                    </div>
-                </body>  
-            </form>
-        </div>
-    );
-}
+                    </body>  
+                </form>
+            </div>
+        );
+    }
 export default LoginPage;
