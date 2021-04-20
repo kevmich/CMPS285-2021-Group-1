75a1c91190da9bef68caa5b3b93d08ecfc6a5280
pudgee123 <71963350+pudgee123@users.noreply.github.com>
2021-04-19 21:51:11 -0500

    Login Change

--- BeautyGardenMain/BeautyGardenMain/beauty/src/Components/LoginPage.js
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/Components/LoginPage.js
@@ -1,59 +1,35 @@
 import React, { useState } from 'react';
 import './LoginPage.css';
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import axios from 'axios';
+import loginForm from '../loginForm';
 
-const Schema = yup.object().shape({
-    userName: yup
-    .string()
-    .required()
-    .min(3),
-
-    password: yup
-    .string()
-    .required()
-    .min(6),
-});
-
-
-
-    function LoginPage() {
-      const{handleSubmit, register, setError} = useForm({validationSchema: Schema});
-
-        function onSubmit(formData) {
-                console.log(formData);
-        }
-
-        console.log(setError)
-
-    
+const loginPage = () => {
+    const {handleChange, login, handleSubmit, isLoading } = loginForm();
 
         return (
             
             <div className="container" >
-                <form className='white' onSubmit={handleSubmit(onSubmit)}>
+                <form className='white' onSubmit={handleSubmit}>
                     <body>                 
                 
                       <div className="input-field">
                             <label htmlFor="userName">User Name</label>
-                            <input type="text" name="userName" placeholder="example@yahoo.com" {...register('userName')} />
-                           {setError.userName && <p>{setError.userName.message}</p>}
+                            <input type="text" name="userName" value={login.userName} onChange={handleChange} placeholder="example@yahoo.com" />
                          </div>   
 
                         <div className="input-field">
                             <label htmlFor="password">Password</label>
-                            <input type="password" name="password" placeholder ="Please enter your password"{...register('password')}
+                            <input type="password" name="password" value={login.password} onChange={handleChange} placeholder ="Please enter your password"
                                 styles={{width:"300px"}} />
-                            {setError.password && <p>{setError.password.message}</p>}
                             </div>
-
-                        <div>
-                            <button className="btn blue darken-3" type="submit">Submit</button>
-                        </div>
+                            <div>
+                        { !isLoading && <button className="btn blue darken-3" type="submit">Submit</button> }
+                        { isLoading && <button className="btn blue darken-3" disabled>
+                            <i className = "fas fa-spinner fa-spin"></i> Submitting...
+                        </button>}
+                    </div>
                     </body>  
                 </form>
             </div>
         );
     }
-export default LoginPage;
+export default loginPage;
