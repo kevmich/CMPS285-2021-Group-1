e25b179307497eb5cbd7dbd6fad3c3a05c95c6c1
Cameron Branch <cameron.branch@selu.edu>
2021-04-22 14:24:56 -0500

    Some Contact Page changes, more coming soon

--- BeautyGardenMain/BeautyGardenMain/beauty/src/Components/NewContact.js
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/Components/NewContact.js
@@ -5,41 +5,69 @@ import './NewContact.css';
 const NewContact = () => {
     const { handleChange, newContact, handleSubmit, isLoading } = useForm();
    
-
-    
-    
-        
-
-       
-    
-
     return (
         
         <div className="container" >
             <form className='white' onSubmit={handleSubmit}>
                 <body>                 
                     <div className="input-field">
-                
-                        <label htmlFor="fullName">Full Name</label>
-                        <input type="text" name="fullName" value={newContact.fullName} onChange={handleChange}  /> 
+                        <label
+                            htmlFor="fullName"
+                            className="fullName"
+                        >Full Name</label>
+                        <input
+                            className="fullNameText"
+                            placeholder="Full Name"
+                            type="text"
+                            name="fullName"
+                            value={newContact.fullName}
+                            onChange={handleChange}
+                        />
                         <div style ={{ color: "red "}}>{newContact.nameError}</div>
                     </div>
                     <div className="input-field">
-                    <label htmlFor="text">Phone Number</label>
-                        <input type="text" name="phoneNumber" maxLength = {10} value={newContact.phoneNumber} onChange={handleChange}  />
+                        <label
+                            htmlFor="phoneNumber"
+                            className="phoneNumber"
+                        >Phone Number</label>
+                        <input
+                            className="phoneNumberText"
+                            placeholder="Phone Number"
+                            type="text"
+                            name="phoneNumber"
+                            maxLength = {10}
+                            value={newContact.phoneNumber}
+                            onChange={handleChange}
+                        />
                         <div style ={{ color: "red "}}>{newContact.phoneError}</div>
                     </div>
                     <div className="input-field" >
-                    <label htmlFor="email">Email</label>
-                        <input type="text" name="email" placeholder="example@gmail.com" value={newContact.email} onChange={handleChange}  />
+                        <label
+                            htmlFor="email"
+                            className="email"
+                        >Email Address</label>
+                        <input
+                            className="emailText"
+                            placeholder="Email Address"
+                            type="text"
+                            name="email"
+                            value={newContact.email}
+                            onChange={handleChange}
+                        />
                         <div style ={{ color: "red "}}>{newContact.emailError}</div>
-                        </div>
-                        <div className="input-field">
-                    <label htmlFor="comment">Comment</label>
-                        <textarea type="text" name="comment" placeholder ="What would you like to know about?" value={newContact.comment}
-                            styles={{width:"300px"}} onChange={handleChange}  />
-                            <div style ={{ color: "red "}}>{newContact.commentError}</div>
-                        </div>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="comment">Comment</label>
+                        <textarea
+                            className="commentText"
+                            placeholder="Is there anything you would like to know about?"
+                            type="text"
+                            name="comment"
+                            value={newContact.comment}
+                            onChange={handleChange}
+                        />
+                        <div style ={{ color: "red "}}>{newContact.commentError}</div>
+                    </div>
                     <div>
                         { !isLoading && <button className="btn blue darken-3" type="submit">Submit</button> }
                         { isLoading && <button className="btn blue darken-3" disabled>
--- BeautyGardenMain/BeautyGardenMain/beauty/src/Contact.js
+++ BeautyGardenMain/BeautyGardenMain/beauty/src/Contact.js
@@ -5,14 +5,19 @@ import './Components/NewContact.css';
 const Contact = () => {
     return (
     <body className="contact_body">
-        <div className="ContactTitle"> 
-            <h1>Contact Us!</h1>
-        </div>
+            <div className="FaceWaxTitles">
+                <div className="FTitleOutlines1">
+                    <div className="FLine1" />
+                </div>
+                <div className="FTitleOutlines2">
+                    <h1>Contact Us</h1>
+                </div>
+                <div className="FTitleOutlines3">
+                    <div className="FLine2" />
+                </div>
+            </div>
         <NewContact />
-       
     </body>
-            
-        
         
     )
 }
